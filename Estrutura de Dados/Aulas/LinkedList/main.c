a78b9022f9a545057910a98d0a4182750d4b3f31
Luis Mario <luismario337y@gmail.com>
2024-04-03 21:07:54 -0300

    Terminando LinkedLists e iniciando Pilhas

--- Estrutura de Dados/Aulas/LinkedList/main.c
+++ Estrutura de Dados/Aulas/LinkedList/main.c
@@ -3,53 +3,135 @@
 
 typedef struct _node {
     int data;
-    struct _node *next;
+    struct _node* next;
 } Node;
 
 typedef struct _linked_list {
-    Node *head;
+    Node* head;
+    Node* tail;
     int size;
 } LinkedList;
 
-Node *new_Node(int data) {
-    Node *node = (Node*) calloc(1, sizeof(Node));
+Node* new_Node(int data) {
+    Node* node = (Node*) calloc(1,sizeof(Node));
     node->data = data;
     node->next = NULL;
     return node;
 }
 
 LinkedList *new_LinkedList() {
-    LinkedList *linkedList = (LinkedList*) calloc(1,sizeof(LinkedList));
+    LinkedList* linkedList = (LinkedList*) calloc(1, sizeof(LinkedList));
     linkedList->head = NULL;
+    linkedList->tail = NULL;
     linkedList->size = 0;
     return linkedList;
 }
 
-//void add(LinkedList **linkedListRef, int data) {}
 
-void LL_addFirst(LinkedList **linkedListRef, int data) {
-    LinkedList *LL = *linkedListRef;
-    Node *nodeRef = new_Node(data);
-    nodeRef->next = LL->head;
-    LL->head = nodeRef;
-    LL->size++;
+void LL_addToHead(LinkedList* linkedListRef, int data) {
+    Node* node = new_Node(data);
+    if(linkedListRef->size == 0) {
+        linkedListRef->tail = node;
+    } else {
+        node->next = linkedListRef->head;
+    }
+    linkedListRef->head = node;
+    linkedListRef->size++;
 }
 
-void LL_print(LinkedList *linkedListRef) {
-    Node *next;
-    next = linkedListRef->head;
-    while(next != NULL) {
-        printf("%i\n", next->data);
-        next = next->next;
+void LL_addToTail(LinkedList* linkedListRef, int data) {
+    if(linkedListRef->size == 0 || linkedListRef->head == NULL) {
+        LL_addToHead(linkedListRef, data);
+    } else {
+        Node* node = new_Node(data);
+        linkedListRef->tail->next = node;
+        linkedListRef->tail = linkedListRef->tail->next;
+        linkedListRef->size++;
     }
+}
+
+Node* LL_getHead(LinkedList* linkedListRef) {
+    return linkedListRef->head;
+}
+
+Node* LL_getTail(LinkedList* linkedListRef) {
+    return linkedListRef->tail;
+}
+
+Node* LL_getIndex(LinkedList* linkedListRef, int index) {
+    Node* aux = linkedListRef->head;
+    for(int i = 0; i < index; i++) {
+        aux = aux->next;
+    }
+    return aux;
+}
+
+void LL_printList(LinkedList* linkedListRef) {
+    if(linkedListRef->size == 0) {
+        printf("Lista Vazia!!!\n");
+    } else {
+        Node* nodeAUX = linkedListRef->head;
+        printf("Head -> ");
+        while(nodeAUX != NULL) {
+            printf("%i -> ", nodeAUX->data);
+            nodeAUX = nodeAUX->next;
+        }
+        printf("Tail!\n");
+    }
+}
+
+int LL_sizeList(LinkedList* linkedListRef) {
+    return linkedListRef->size;
+}
+
+void LL_removeElement(LinkedList* linkedListRef, int data) {
+    if(linkedListRef->size != 0) {
+        Node* prev = NULL;
+        Node* pos = linkedListRef->head;
+        while(pos != NULL && pos->data != data) {
+            prev = pos;
+            pos = pos->next;
+        }
+
+        if(pos != NULL) {
+            if(linkedListRef->tail == pos) {
+                linkedListRef->tail = prev;
+            }
+            if(linkedListRef->head == pos) {
+               linkedListRef->head = linkedListRef->head->next ;
+            }
+            else {
+                prev->next = pos->next;
+            }
+            free(pos);
+            linkedListRef->size--;
+        }
+    }
+}
+
+void LL_destroyLinkedList(LinkedList** linkedListRef) {
+    printf("Destruindo LinkedList!\n");
+    LinkedList* _LL = *linkedListRef;
+    Node* aux = _LL->head;
+    Node* deleted = NULL;
+    while(aux != NULL) {
+        deleted = aux;
+        aux = aux->next;
+        free(deleted);
+    }
+    free(_LL);
+    *linkedListRef = NULL;
 }
 
 int main() {
     LinkedList *myLL = new_LinkedList();
-    LL_addFirst(&myLL, 5);
-    LL_addFirst(&myLL, 10);
-    LL_addFirst(&myLL, 15);
-    LL_addFirst(&myLL, 20);
-    LL_print(myLL);
+    LL_addToHead(myLL, 5);
+    LL_addToHead(myLL, 10);
+    LL_addToHead(myLL, 15);
+    LL_addToHead(myLL, 20);
+    LL_addToTail(myLL, 19);
+    LL_printList(myLL);
+    LL_removeElement(myLL, 15);
+    LL_printList(myLL);
     return 0;
 }
---
+++ Estrutura de Dados/Aulas/Pilhas/Pilhas Estaticas/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE_STACK 10
+
+typedef struct _stack {
+    int *data;
+    int capacity;
+    int size;
+} Stack;
+
+Stack* new_Stack(int capacity) {
+    Stack* newStack = (Stack*) calloc(1, sizeof(Stack));
+    newStack->size = 0;
+    newStack->capacity = capacity;
+    newStack->data = (int*) calloc(newStack->capacity, sizeof(int));
+    return newStack;
+}
+
+int S_isEmpty(Stack* stackRef) {
+    if(stackRef->size == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int S_isFull(Stack* stackRef) {
+    if(stackRef->size == stackRef->capacity) {
+        return 1;
+    }
+    return 0;
+}
+
+void S_push(Stack* stackRef, int number) {
+    if(S_isFull(stackRef)) {
+        printf("Pilha esta Cheia!!!\n");
+    } 
+    else {
+        stackRef->data[stackRef->size] = number;
+        stackRef->size++;
+    }
+}
+
+void S_pop(Stack* stackRef) {
+    if(S_isEmpty(stackRef)) {
+        printf("Pilha esta vazia!\n");
+    }
+    else {
+        stackRef->data[stackRef->size] = 0;
+        stackRef->size--;
+    }
+}
+
+void S_peek(Stack* stackRef) {
+    if(S_isEmpty(stackRef)) {
+        printf("Pilha esta vazia!\n");
+    } 
+    else {
+        printf("Elemento do Topo: %i\n", stackRef->data[stackRef->size - 1]);
+    }
+}
+
+void S_destroy(Stack** stackRef) {
+    Stack* stack = *stackRef;
+    free(stack->data);
+    free(stack);
+    *stackRef = NULL;
+}
+
+int main() {
+    Stack* myStack = new_Stack(3);
+    S_push(myStack, 10);
+    S_push(myStack, 20);
+    S_push(myStack, 30);
+    S_push(myStack, 40);
+    S_push(myStack, 50);
+    S_pop(myStack);
+    S_peek(myStack);
+    return 0;
+}
